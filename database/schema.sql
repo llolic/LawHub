f0808ac40cc33efb693cbd76561e9648c9164658
Lazar Lolic <lazar.lolic@mail.utoronto.ca>
2020-02-11 18:14:59 -0500

    Edit app user table and add database information

--- database/schema.sql
+++ database/schema.sql
@@ -1,13 +1,21 @@
-create table User (
+create table AppUser (
     username varchar(100) NOT NULL primary key,
     password varchar(100) NOT NULL, 
     firstName varchar(100) NOT NULL,
     lastName varchar(100) NOT NULL,
     email varchar(100) NOT NULL,
-    role ENUM('student', 'recruiter') NOT NULL,
+    role userType,
     country varchar(100) NOT NULL,
     stateOrProvince varchar(100) NOT NULL,
     city varchar(100) NOT NULL
 );
 
+--userType is a custom type that is ENUM('student', 'recruiter')
+
 -- INSERT INTO User VALUES($1, $2, $3, $4, $5, 'student', $6, $7, $8);
+
+-- 34.66.215.42
+-- port: 5432
+-- database name: 'lh_db'
+-- user: 'user1'
+-- password: 'Lamas123'
