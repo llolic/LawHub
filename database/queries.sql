b66589133cb5a1dfbfc7b57e1b38ef1895e0a29e
Lazar Lolic <lazar.lolic@mail.utoronto.ca>
2020-03-10 17:42:50 -0400

    [LAM-10] LAM-54 fix query

--- database/queries.sql
+++ database/queries.sql
@@ -1,4 +1,4 @@
 -- LAM-10: Filter students
-SELECT AppUser.uid, firstName, lastName FROM AppUser RIGHT JOIN Student ON AppUser.uid=Student.uid WHERE studyLevel={} OR school={} OR country={} OR stateOrProvince={} OR city={};
+SELECT AppUser.uid, firstName, lastName FROM AppUser RIGHT JOIN Student ON AppUser.uid=Student.uid WHERE studyLevel={} AND school={} AND country={} AND stateOrProvince={} AND city={};
 
 -- Notes: The query takes in a studyLevel, school, country, stateOrProvince, and city, and returns all students that satisfy that criteria (uid, firstName, lastName)
