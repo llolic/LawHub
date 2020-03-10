3eef6933e739283462db1d1585091511942a5519
Lazar Lolic <lazar.lolic@mail.utoronto.ca>
2020-03-10 17:36:57 -0400

    [LAM-9] LAM-53 Fix query

--- database/queries.sql
+++ database/queries.sql
@@ -1,6 +1,6 @@
 -- LAM-9: Quiz Filter
-(SELECT DISTINCT Quiz.quizId, title, numQuestions FROM HasTags RIGHT JOIN Quiz ON HasTags.quizId=Quiz.quizId WHERE tag={} OR tag={} OR tag={}) UNION
-(SELECT DISTINCT quizId, title, numQuestions FROM Quiz WHERE author={}) UNION
+(SELECT DISTINCT Quiz.quizId, title, numQuestions FROM HasTags RIGHT JOIN Quiz ON HasTags.quizId=Quiz.quizId WHERE tag={} OR tag={} OR tag={}) INTERSECT
+(SELECT DISTINCT quizId, title, numQuestions FROM Quiz WHERE author={}) INTERSECT
 (SELECT DISTINCT quizId, title, numQuestions FROM Quiz WHERE title LIKE '%{}%');
 
 -- Notes: takes 3 tags, an author, and a string and returns a list of {quizId, title, numQuestions}
