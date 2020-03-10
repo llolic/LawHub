4a1c15c4652f57655c633fd8b56f966f29b9f9fa
Lazar Lolic <lazar.lolic@mail.utoronto.ca>
2020-03-09 20:54:01 -0400

    [LAM-7] LAM-50 Add notes

--- database/queries.sql
+++ database/queries.sql
@@ -1,3 +1,5 @@
 -- LAM-7: Quiz leaderboard
 SELECT QuizRecord.uid, score, firstName, lastName FROM QuizRecord RIGHT JOIN AppUser ON QuizRecord.uid=AppUser.uid WHERE quizId={} ORDER BY score DESC LIMIT {};
 SELECT title FROM Quiz WHERE quizId={};
+
+-- Notes: First query takes in a quizId and a limit and returns a list of {uid, score, firstName, lastName} in descending order limited to the specified number of rows.
