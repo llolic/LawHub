8c903e92055842047e9cd603700eb88b33227d6a
Lazar Lolic <lazar.lolic@mail.utoronto.ca>
2020-03-13 18:42:29 -0400

    Fix fetch quiz query to add Question.questionId to the second query because of the duplicate column

--- database/queries.sql
+++ database/queries.sql
@@ -6,7 +6,7 @@ SELECT AppUser.uid, firstName, lastName FROM AppUser RIGHT JOIN Student ON AppUs
 
 -- Fetch Quiz
 SELECT title, author, numQuestions FROM QUIZ WHERE quizId={};
-SELECT questionId, questionType, option1, option2, option3, option4, correctAnswer FROM Question RIGHT JOIN QuizContains ON Question.questionId=QuizContains.questionId WHERE quizId={};
+SELECT Question.questionId, questionType, option1, option2, option3, option4, correctAnswer FROM Question RIGHT JOIN QuizContains ON Question.questionId=QuizContains.questionId WHERE quizId={};
 
 -- Fetch Quiz List
 SELECT quizId, title FROM Quiz;
