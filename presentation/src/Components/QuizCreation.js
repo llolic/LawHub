1009d95b8870428bbd62c9a334a6e50a7fc8136d
Alfonso Dela Cruz <alfonso.delacruz@mail.utoronto.ca>
2020-03-13 16:16:47 -0400

    [LAM-45] Small optimization changes

--- presentation/src/Components/QuizCreation.js
+++ presentation/src/Components/QuizCreation.js
@@ -110,7 +110,7 @@ class QuizCreation extends React.Component {
   getQuestionDisplay = i => {
     let display = [];
     if (this.state.questions[i].questionType === "") {
-      display.push(<div>nothing</div>);
+      display.push(<div></div>);
       return display;
     }
     if (this.state.questions[i].questionType !== "2") {
@@ -170,9 +170,9 @@ class QuizCreation extends React.Component {
         ];
         display.push(
           <div>
-            Question{j + 1}: {this.state.response[j].question}
+            Question {j + 1}: {this.state.response[j].question}
             <br></br>
-            Answer{j + 1}: {options[this.state.response[j].correctAnswer]}
+            Answer {j + 1}: {options[this.state.response[j].correctAnswer]}
             <Button 
               text="Use this question"
               onClick={e => this.usePremadeQuestion(i, this.state.response[j].questionId)} //TODO: update backend to expect incoming quesitonId
