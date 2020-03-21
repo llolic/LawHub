be4df0a554e3d232ae8979d81b25db6359cc586a
Lazar Lolic <lazar.lolic@mail.utoronto.ca>
2020-03-21 12:43:05 -0400

    [LAM-12] LAM-69 Add query to get information for a specific recruiter

--- database/queries.sql
+++ database/queries.sql
@@ -37,3 +37,9 @@ SELECT firstName, lastName, email, country, stateOrProvince, city, studyLevel, s
 
 -- Notes: First query gets all the quiz records associated with a specified student
 --        Second query gets all the information about a specified student
+
+
+
+
+-- LAM-12: view recruiter profile and postings
+SELECT firstName, lastName, company, title, bio, stateOrProvince, country FROM AppUser INNER JOIN Recruiter ON AppUser.uid=Recruiter.uid WHERE Recruiter.uid={};
