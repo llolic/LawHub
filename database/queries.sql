b8ed0d6af24d07b4bf3ae9420b5f95fff5478074
Lazar Lolic <lazar.lolic@mail.utoronto.ca>
2020-03-21 12:25:53 -0400

    [LAM-4] LAM-65 Add query to get information for all postings with a specific stateOrProvince

--- database/queries.sql
+++ database/queries.sql
@@ -37,3 +37,9 @@ SELECT firstName, lastName, email, country, stateOrProvince, city, studyLevel, s
 
 -- Notes: First query gets all the quiz records associated with a specified student
 --        Second query gets all the information about a specified student
+
+
+
+
+-- LAM-4: posting suggestions
+SELECT DISTINCT postingId, title, description, Posting.stateOrProvince, firstName, lastName FROM Posting RIGHT JOIN AppUser ON Posting.recruiterId=AppUser.uid WHERE Posting.stateOrProvince={};
